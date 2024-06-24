1c35daa0bf8810e1932cb923d41bb8b3a6e70a99
nimisha-gj <156162253+nimisha-gj@users.noreply.github.com>
2024-06-24 08:56:14 -0800

    refactor (exercise-1): provides local variables to ec2 module

---
+++ ec2-terraform-excercise/locals.tf
@@ -0,0 +1,7 @@
+locals {
+  region = "ap-south-1"
+  ami_id = "ami-0e1d06225679bc1c5"
+  instance_type = "t2.micro"
+  instance_name = "ec2-exercise-1"
+  user = "nimisha"
+}
--- ec2-terraform-excercise/main.tf
+++ ec2-terraform-excercise/main.tf
@@ -1,3 +1,6 @@
 module "ec2" {
   source = "./modules/ec2"
+  aws_ami_id = local.ami_id
+  aws_instance_name = local.instance_name
+  aws_instance_type = local.instance_type
 }
