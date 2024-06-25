0745d0f6e32a5c5b4f33555bd800f69b92428f45
nimisha-gj <156162253+nimisha-gj@users.noreply.github.com>
2024-06-24 21:37:42 -0800

    wip (exercise-2): adds security group module to allow SSH connections

---
+++ ec2-sg-terraform/modules/ssh-sg/main.tf
@@ -0,0 +1,21 @@
+resource "aws_security_group" "demo_sg" {
+  description = "Allow inbound traffic on port 22"
+  vpc_id      = var.vpc_id
+
+  ingress {
+    from_port   = 22
+    to_port     = 22
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+  egress {
+    from_port   = 0
+    to_port     = 0
+    protocol    = "-1"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  tags = {
+    Name = var.sg_name
+  }
+}
---
+++ ec2-sg-terraform/modules/ssh-sg/variables.tf
@@ -0,0 +1,9 @@
+variable "sg_name" {
+  description = "Name of the Security group"
+  type = string
+}
+
+variable "vpc_id" {
+  description = "Id of the VPC"
+  type = string
+}
