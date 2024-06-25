be4f38fe0a25ee31e7bc4f76032142b7779f3d2c
nimisha-gj <156162253+nimisha-gj@users.noreply.github.com>
2024-06-25 03:48:07 -0800

    wip (exercise-2): adds security groups module

---
+++ ec2-sg-terraform/modules/sg/nat-traffic/main.tf
@@ -0,0 +1,21 @@
+resource "aws_security_group" "exercise2_all_traffic_sg" {
+  description = "Allow all traffic on all ports"
+  vpc_id      = var.vpc_id
+
+  ingress {
+    from_port   = 0
+    to_port     = 0
+    protocol    = "-1"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+  egress {
+    from_port   = 0
+    to_port     = 0
+    protocol    = "-1"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  tags = {
+    Name = var.sg_name
+  }
+}
---
+++ ec2-sg-terraform/modules/sg/nat-traffic/output.tf
@@ -0,0 +1,3 @@
+output "sg_id" {
+  value = aws_security_group.exercise2_all_traffic_sg.id
+}
