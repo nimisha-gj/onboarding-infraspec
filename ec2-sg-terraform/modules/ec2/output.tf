fdc433e03c3e5e90000aa0703b87d84a3c2d9a4b
nimisha-gj <156162253+nimisha-gj@users.noreply.github.com>
2024-06-25 04:07:50 -0800

    chore (exercise-2): adds network interface id ouput

--- ec2-sg-terraform/modules/ec2/output.tf
+++ ec2-sg-terraform/modules/ec2/output.tf
@@ -2,3 +2,7 @@ output "instance_id" {
   description = "EC2 instance id"
   value = aws_instance.exercise_2_instance.id
 }
+
+output "network_interface_id" {
+  value = aws_instance.exercise_2_instance.primary_network_interface_id
+}
