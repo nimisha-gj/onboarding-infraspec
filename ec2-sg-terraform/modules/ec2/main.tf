8ace1772b2a03b7627c29cf1853feb3f030dabe0
nimisha-gj <156162253+nimisha-gj@users.noreply.github.com>
2024-06-25 04:06:52 -0800

    refactor (exercise-2): assigns multiple security groups to ec2

--- ec2-sg-terraform/modules/ec2/main.tf
+++ ec2-sg-terraform/modules/ec2/main.tf
@@ -2,7 +2,7 @@ resource "aws_instance" "exercise_2_instance" {
   ami                         = var.aws_ami_id
   instance_type               = var.aws_instance_type
   subnet_id                   = var.aws_subnet_id
-  vpc_security_group_ids      = [var.aws_sg_id]
+  vpc_security_group_ids      = var.aws_sg_ids
   key_name                    = var.aws_key_name
   associate_public_ip_address = var.associate_public_ip_address
   source_dest_check           = var.source_dest_check
